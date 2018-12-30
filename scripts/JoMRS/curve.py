76ff6f4bad7ec1c40a7bebfd1ca48da85d1bddf5
Johannes Wolz <johanneswolz@gmail.com>
2018-12-30 20:02:02 +0100

    added retun control

--- scripts/JoMRS/curve.py
+++ scripts/JoMRS/curve.py
@@ -49,3 +49,4 @@ def controls(name, typ='box', position=None):
                         k=[0, 1, 2, 3, 4, 5, 6, 7, 8,
                            9, 10, 11, 12, 13, 14, 15],
                         n=name)
+    return control
