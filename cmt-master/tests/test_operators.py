cdf1904637e834fca8c2b814a78e5fa197b05fc0
johannes wolz <wolzjohannes@gmail.com>
2019-08-15 22:36:26 +0200

    wip

--- cmt-master/tests/test_operators.py
+++
@@ -1,8 +0,0 @@
-import operators
-from cmt.test import TestCase
-
-class TestExample(TestCase):
-
-    def test_create_main_op(self):
-        op = operators.create_component_operator()
-        print op
--- scripts/JoMRS/tests/mayaunittest.py
+++ scripts/JoMRS/tests/mayaunittest.py
@@ -18,7 +18,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
+# Base code from Chad Vernon cmt_master.
 # Author:     Johannes Wolz / Rigging TD
 # Date:       2019 / 08 / 12
 
@@ -28,6 +28,8 @@ Module for python unittest for maya.
 import os
 import logging
 import logger
+import unittest
+import sys
 
 ##########################################################
 # GLOBALS
@@ -39,14 +41,59 @@ module_logger = logging.getLogger(__name__ + ".py")
 # FUNCTIONS
 ##########################################################
 
-def get_dir_module_tests():
-    """Generator function to iterate over all the Maya module tests directories."""
-    p = "{0}/scripts/JoMRS/tests".format(os.environ["JoMRS"])
-    if os.path.exists(p):
-            return p
+def get_module_tests_path():
+    """
+    Generator function to get the unittest directory path
+    Return:
+                Path to unittest directory.
+    """
+    path = "{0}/scripts/JoMRS/tests".format(os.environ["JoMRS"])
+    if os.path.exists(path):
+            return path
     else:
         logger.log(level='error', message="Directory for unittest not "
                                           "exist", logger=module_logger)
+
+def get_tests(directory=None, test=None, test_suite=None):
+
+    if directory is None:
+        directory = get_module_tests_path()
+
+    if test_suite is None:
+        test_suite = unittest.TestSuite()
+
+    if test:
+        dir_add_to_syspath = [p for p in [directory] if add_to_syspath(p)]
+        discovered_suite = unittest.TestLoader().loadTestsFromName(test)
+        if discovered_suite.countTestCases():
+            test_suite.addTests(discovered_suite)
+    else:
+        # Find all tests to run
+        dir_add_to_syspath = []
+        for p in [directory]:
+            discovered_suite = unittest.TestLoader().discover(p)
+            if discovered_suite.countTestCases():
+                test_suite.addTests(discovered_suite)
+
+    # Remove the added paths.
+    for path in dir_add_to_syspath:
+        sys.path.remove(path)
+
+    return test_suite
+
+def add_to_syspath(path):
+    """
+    Add the specified path to the system path.
+     Args:
+            path(str) Path to add.
+    Return:
+            True if path was added. Return false if path does not exist or
+            path was already in sys.path
+    """
+    if os.path.exists(path) and path not in sys.path:
+        sys.path.insert(0, path)
+        return True
+    return False
 
 def run_tests_from_commandline():
     """Runs the tests in Maya standalone mode.
