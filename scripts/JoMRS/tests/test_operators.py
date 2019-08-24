e1bf80ceb0541445596661e11b86e670e54a82b5
johannes wolz <wolzjohannes@gmail.com>
2019-08-24 23:06:58 +0200

    test_operator wip

--- scripts/JoMRS/tests/test_operators.py
+++ scripts/JoMRS/tests/test_operators.py
@@ -1,3 +1,6 @@
+"""
+JoMRS operator unittest module
+"""
 import operators
 import pymel.core as pmc
 import pymel.core.datatypes as dt
@@ -5,7 +8,14 @@ from tests.mayaunittest import TestCase
 
 
 class TestOperators(TestCase):
+    """
+    Test the operators module.
+    """
+
     def setUp(self):
+        """
+        Setup variables for all other tests.
+        """
         spine_op_instance = operators.create_component_operator()
         self.spine_op = spine_op_instance.build_node(
             operator_name="spine", side="M"
@@ -21,9 +31,9 @@ class TestOperators(TestCase):
             .sub_operator_nd.get()
         )
         self.spine_main_meta_nd = self.spine_main_op.main_op_meta_nd.get()
-        self.spine_main_meta_nd.component_name.set('spine')
-        self.spine_main_meta_nd.component_type.set('spine_component')
-        self.spine_main_meta_nd.component_side.set('M')
+        self.spine_main_meta_nd.component_name.set("spine")
+        self.spine_main_meta_nd.component_type.set("spine_component")
+        self.spine_main_meta_nd.component_side.set("M")
         self.spine_main_op.rotateZ.set(90)
         self.spine_main_op.translateY.set(15)
         pmc.select(self.spine_sub_op)
@@ -42,10 +52,12 @@ class TestOperators(TestCase):
             .sub_operator_nd.get()
         )
         pmc.select(clear=True)
-        self.clavicle_main_meta_nd = self.clavicle_main_op.main_op_meta_nd.get()
-        self.clavicle_main_meta_nd.component_name.set('clavicle')
-        self.clavicle_main_meta_nd.component_type.set('clavicle_component')
-        self.clavicle_main_meta_nd.component_side.set('L')
+        self.clavicle_main_meta_nd = (
+            self.clavicle_main_op.main_op_meta_nd.get()
+        )
+        self.clavicle_main_meta_nd.component_name.set("clavicle")
+        self.clavicle_main_meta_nd.component_type.set("clavicle_component")
+        self.clavicle_main_meta_nd.component_side.set("L")
         self.clavicle_main_op.rotateZ.set(-90)
         self.clavicle_main_op.translateY.set(-2)
         pmc.select(self.clavicle_sub_op)
@@ -71,26 +83,58 @@ class TestOperators(TestCase):
         self.arm_main_op.rotateZ.set(-45)
         pmc.select(clear=True)
         self.arm_main_meta_nd = self.arm_main_op.main_op_meta_nd.get()
-        self.arm_main_meta_nd.component_name.set('arm')
-        self.arm_main_meta_nd.component_type.set('arm_component')
-        self.arm_main_meta_nd.component_side.set('L')
+        self.arm_main_meta_nd.component_name.set("arm")
+        self.arm_main_meta_nd.component_type.set("arm_component")
+        self.arm_main_meta_nd.component_side.set("L")
 
     def test_spine_operator(self):
-        world_space = self.spine_main_op.getTranslation(space='world')
+        """
+        Test the spine operator.
+        """
+        world_space = self.spine_main_op.getTranslation(space="world")
         self.assertEqual(world_space, dt.Vector([0.0, 15.0, 0.0]))
-        self.assertEqual(self.spine_main_meta_nd.component_name.get(),
-                         'spine')
-        self.assertEqual(self.spine_main_meta_nd.component_type.get(),
-                         'spine_component')
-        self.assertEqual(self.spine_main_meta_nd.component_side.get(),
-                         'M')
+        self.assertEqual(
+            self.spine_main_meta_nd.component_name.get(), "spine"
+        )
+        self.assertEqual(
+            self.spine_main_meta_nd.component_type.get(), "spine_component"
+        )
+        self.assertEqual(self.spine_main_meta_nd.component_side.get(), "M")
         self.assertEqual(
             self.spine_main_op, pmc.PyNode("M_MAIN_op_spine_0_CON")
         )
 
     def test_clavicle_operator(self):
-        world_space = self.clavicle_main_op.getTranslation(space='world')
-        self.assertEqual(world_space, dt.Vector([2.000000000000002, 25.0,
-                                                  0.0]))
-        object_space = self.clavicle_main_op.getTranslation(space='object')
+        """
+        Test the clavicle operator.
+        """
+        world_space = self.clavicle_main_op.getTranslation(space="world")
+        self.assertEqual(
+            world_space, dt.Vector([2.000000000000002, 25.0, 0.0])
+        )
+        object_space = self.clavicle_main_op.getTranslation(space="object")
         self.assertEqual(object_space, dt.Vector([0.0, -2.0, 0.0]))
+        self.assertEqual(
+            self.clavicle_main_op, pmc.PyNode("L_MAIN_op_clavicle_0_CON")
+        )
+        self.assertEqual(
+            self.clavicle_main_op.main_op_meta_nd.get(),
+            self.clavicle_main_meta_nd,
+        )
+        self.assertEqual(
+            self.clavicle_main_op.getParent(generations=2), self.spine_sub_op
+        )
+
+    def test_god_meta_nd(self):
+        """
+        Test scene god node.
+        """
+        self.assertTrue(pmc.PyNode("god_meta_0_METAND"))
+        meta_nodes = []
+        for x in range(8):
+            meta_nodes.append(
+                pmc.PyNode("god_meta_0_METAND")
+                .attr("meta_nd_{}".format(str(x)))
+                .get()
+            )
+        self.assertIs(len(meta_nodes), 8)
